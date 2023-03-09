562074f1185807672278c4e922c433e782fd16fa
diegoram23 <diego.pta.oc@gmail.com>
2023-03-09 10:19:32 -0800

    added morgan middleware

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 //listen for requests
-app.listen(3000)
+app.listen(3003)
+
+// middleware
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>')
-    res.sendFile('./views/index.html', { root:__dirname} )
+    res.sendFile('./views/index.html', { root: __dirname })
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root:__dirname })
+    res.sendFile('./views/about.html', { root: __dirname })
 })
 
 //redirects
-
 app.get('/about-us', (req, res) => {
     res.redirect('/about')
 })
 
 //404 page
-
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root:__dirname } )
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
